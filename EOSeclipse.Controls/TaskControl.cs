997d5f42968486bcf5bb1c8d0b3e0d0a08f49e04
LaFeev <Aaron.LaFevers@gmail.com>
2024-01-09 13:59:35 -0600

    Create TaskControl class

---
+++ EOSeclipse.Controls/TaskControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EOSeclipse.Controls
+{
+    public partial class TaskControl : UserControl
+    {
+        public double Av { get; set; }
+        public string Tv { get; set; }
+        public int ISO { get; set; }
+        public int AEB { get; set; }
+        public string Script { get; set; }
+
+        public TaskControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
